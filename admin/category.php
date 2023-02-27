95df96dab54373aaf54f24e5eed0e819d5b9e3c6
dangchung02 <91543141+dangchung02@users.noreply.github.com>
2023-02-27 10:31:37 +0700

    update capnhatSL

--- admin/category.php
+++ admin/category.php
@@ -22,7 +22,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="./">Trang chủ</a>
+                        <a class="nav-link" aria-current="page" href="home.php">Trang chủ</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="../index.php">Trang ngoài</a>
